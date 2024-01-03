32e03c39d8ced48d807fb328a3480b6a51ff62fe
Hajar Sabt <hajarsabt@gmail.com>
2024-01-03 22:27:08 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:ocr_application/pages/home.dart';
+import 'package:ocr_application/pages/upload.dart';
+import 'package:ocr_application/utils/routes.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "SWIFT Lens App",
+
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      color: Colors.white,
+      // initialRoute: '/',
+      routes: {
+        MyRoutes.home: (context) => HomePage(),
+        MyRoutes.uploadpage: (context) => UploadPage(),
+      },
+    );
+  }
+}
